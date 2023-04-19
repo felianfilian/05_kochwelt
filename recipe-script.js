79a9f4d54e2302aedfbc0c6e2d216c7280c6192f
felianfilian <darianbellentor@outlook.de>
2023-04-19 19:33:40 +0200

    update 230419

--- recipe-script.js
+++ recipe-script.js
@@ -1,7 +1,8 @@
-let = [
+let recipes = [
   {
-    "recipeName": "Griechischer Bauernsalat",
-    "ingredients": [
+    recipeName: "Griechischer Bauernsalat",
+    recipeImg: "img/salad.jpg",
+    ingredients: [
       " Salatgurke",
       " Paprikaschoten (rot und grün)",
       "g Tomaten",
@@ -13,31 +14,146 @@ let = [
       "ml Olivenöl",
       "Oregano",
     ],
-    "ppAmount": [0.25, 0.5, 125, 0.5, 50, 25, 0, 0.25, 25, 0]
-  }
-]
-
-// let ppAmount = [0.25, 0.5, 125, 0.5, 50, 25, 0, 0.25, 25, 0];
-
-// let ingredients = [
-//   " Salatgurke",
-//   " Paprikaschoten (rot und grün)",
-//   "g Tomaten",
-//   " Zwiebeln",
-//   "g Schafskäse",
-//   "g Oliven aus dem Glas",
-//   "Salz und Pfeffer",
-//   " Zitrone",
-//   "ml Olivenöl",
-//   "Oregano",
-// ];
+    ppAmount: [0.25, 0.5, 125, 0.5, 50, 25, 0, 0.25, 25, 0],
+    recipeInstructions: `
+    <br>
+    Paprika waschen, entkernen und in dünne Streifen schneiden.
+    <br>
+    Tomaten waschen und achteln. Zwiebeln schälen und in feine Ringe schneiden.
+    <br>
+    Schafskäse würfeln und mit Oregano besteuen.
+    <br>
+    Oliven abgießen und mit Gurke, Paprika, Tomaten, Zwiebeln und Schafskäse in eien Schüssel geben.
+    <br>
+    Olivenöl, Zitronensaft, Salz und Pfeffer zu einer Soße verrühren und über den Salat gießen. Umrühren
+    `,
+    avatarImg: "img/avatar-dennis.jpg",
+    avatarName: "Dennis",
+  },
+  {
+    recipeName: "Spaghetti Bolognese",
+    recipeImg: "img/bolognese.jpg",
+    ingredients: [
+      " Zwiebel(n)",
+      " Möhre",
+      " Rinderhackfleisch oder Tartar",
+      " Salz und Pfeffer",
+      "ml Gemüsebrühe (Instant)",
+      "g Tomatenmark",
+      "TL Oregano",
+      " g Tomaten, stückige, mit Kräutern",
+      " EL Tomatenketchup",
+      " g Spaghetti",
+    ],
+    ppAmount: [0.25, 0.25, 125, 0, 50, 20, 0.25, 100, 0.5, 125],
+    recipeInstructions: `
+    Schäle Zwiebeln, Knoblauch und Möhren und schneide sie in kleine Stücke.
+      <br/>
+      Gib das Hackfleisch in eine Pfanne und brate es langsam im eigenen Fett
+      an.
+      <br/>
+      Würze es mit Salz und Pfeffer.
+      <br/>
+      Gib die geschnittenen Zwiebeln, Knoblauch und Möhren hinzu und brate alles
+      kurz mit.
+      <br/>
+      Füge Brühe hinzu und rühre Tomatenmark, Oregano, stückige Tomaten und
+      Tomatenketchup unter.
+      <br/>
+      Koche alles für etwa 40 Minuten ein.
+      <br/>
+      Koche die Spaghetti in gesalzenem Wasser, bis sie bissfest sind.
+      <br/>
+      Gieße sie durch ein Sieb ab und spüle sie mit kaltem Wasser ab.
+      <br/>
+      Serviere sie zusammen mit der Sauce.
+    `,
+    avatarImg: "img/profile.png",
+    avatarName: "Frieder",
+  },
+  {
+    recipeName: "Pad Thai",
+    recipeImg: "img/pexels-padthai.jpg",
+    ingredients: [
+      " Schalotten",
+      " Knoblauchzehen",
+      " Frühlingszwiebel",
+      "g fester Tofu",
+      " Ei(er)",
+      " EL geröstete Cashewkerne",
+      "g Mungobohnensprossen",
+      " Limette",
+      "<h2>Sauce</h2>",
+      " EL Kokosblütenzucker",
+      " TL Tamarindenpaste",
+      " EL Fischsauce",
+      " TL Rote Chiliflocken",
+      " EL Öl zum Braten",
+      " mittelgroße rohe Garnelen",
+    ],
+    ppAmount: [
+      0.5, 0.5, 0.25, 25, 0.5, 0.5, 25, 0.25, 0, 0.25, 0.25, 1, 0.125, 0.5, 2,
+    ],
+    recipeInstructions: `
+    Die Nudeln in lauwarmem Wasser 15 Minuten einweichen..
+    <br>
+    Die Schalotten schälen, halbieren und in feine Ringe schneiden.
+    Den Knoblauch schälen und feinhacken. Das Grüne der Frühlingszwiebel in 3cm lange Stücke schneiden, diese je nach
+    Größe längs halbieren oder vierteln.
+    Das Weiße fein hacken.
+    <br>
+    Den Tofu in 0,5cm Würfel schneiden. Die Eier in einer Schale aufschlagen und verquirlen. Die Erdnüsse im Mörser grob
+    zerstoßen.
+    Die Mungbohnensprossen waschen und putzen. Die Limette in Achtel schneiden.
+    <br>
+    Die Nudeln aus dem Wasser nehmen und abtropfen lassen.
+    <br>
+    Für die Saucenmischung Kokosblütenzucker, Tamarindenmark, Fischsauce und die Chiliflocken zusammen mit 200ml Wasser
+    in einer Schale verrühren.
+    <br>
+    Zum Braten am besten eine flache Antihaftpfanne mit 28cm Durchmesser verwenden. Damit kann man verhindern, dass das
+    Ei anbrennt.
+    Außerdem einen flachen Holzspatel verwenden.
+    <br>
+    Die Pfanne auf mittlerer Stufe erhitzen, 2 EL Öl hineingeben.
+    Die Schalotten und das Weiße der Frühlingszwiebeln andünsten, nach 30 Sekunden den Knoblauch hinzugeben.
+    Darauf achten, dass nichts braun wird. Den Tofu dazugeben und unter Rühren weitere 30 Sekunden braten. Jetzt die
+    Nudeln hinzugeben und alles gründlich vermischen.
+    ⅔ der Saucenmischung dazugeben und wieder gründlich vermischen.
+    <br>
+    Die Nudeln sollten jetzt weich werden, aber nicht matschig.
+    Sie dürfen aber auch nicht mehr zäh sein. Gegebenenfalls noch von der restlichen Saucenmischung oder etwas heißes
+    Wasser zugeben.
+    <br>
+    Mit dem Holzspatel die Nudelmasse in eine Hälfte der Pfanne schieben.
+    Auf die freie Fläche einen Spritzer Öl geben, die Garnelen hinzugeben und von beiden Seiten 30 Sekunden anbraten.
+    Anschließend auf die Seite mit den Nudeln schieben.
+    <br>
+    Wieder einen Spritzer Öl auf die freie Fläche geben und die Eimasse hineingleiten lassen, kurz stocken lassen und
+    dann mit dem Spatel zerteilen.
+    Es sollte mehr einem Rührei als einem Omelett ähneln. Sobald das Ei zu einer festen Masse geworden ist, mit der
+    Nudelmasse vermengen.
+    <br>
+    Schliesslich die Mungbohnensprossen, die Erdnüsse und das Grüne der Frühlingszwiebeln hinzufügen.
+    Alles nochmals gründlich vermischen und solange weiter braten, bis die Frühlingszwiebeln etwas weich geworden sind.
+    Servieren und mit Limette abschmecken. Eventuell noch mit etwas Chiliflocken nachwürzen.
+    `,
+    avatarImg: "img/avatar-pauline.jpg",
+    avatarName: "Pauline",
+  },
+];
 
 let ingredientsContainer = document.getElementById("ingredients");
 
 let defaultPersonCount = 4;
 let personCount = document.getElementById("person-count");
 
-function init() {
+function change(index) {
+  recipeIndex = index;
+  showIngredients(defaultPersonCount);
+}
+
+function init(recipeIdent) {
   personCount.value = 4;
   showIngredients(defaultPersonCount);
 }
@@ -52,6 +168,16 @@ function changePersons() {
 
 function showIngredients(personMultiplier) {
   ingredientsContainer.innerHTML = "";
+
+  let recipeTitle = document.getElementById("recipe-title");
+  recipeTitle.innerHTML = recipes[recipeIndex]["recipeName"];
+
+  let recipeImg = document.getElementById("recipe-img");
+  recipeImg.src = recipes[recipeIndex]["recipeImg"];
+
+  let ingredients = recipes[recipeIndex]["ingredients"];
+  let ppAmount = recipes[recipeIndex]["ppAmount"];
+
   let actualAmount;
   for (let i = 0; i < ingredients.length; i++) {
     if (ppAmount[i] == 0) {
@@ -61,4 +187,13 @@ function showIngredients(personMultiplier) {
     }
     ingredientsContainer.innerHTML += `<li>${actualAmount} ${ingredients[i]}</li>`;
   }
+
+  let recipeInstructions = document.getElementById("recipe-instructions");
+  recipeInstructions.innerHTML = recipes[recipeIndex]["recipeInstructions"];
+
+  let avatarImg = document.getElementById("avatar-img");
+  avatarImg.src = recipes[recipeIndex]["avatarImg"];
+
+  let avatarName = document.getElementById("avatar-name");
+  avatarName.innerHTML = recipes[recipeIndex]["avatarName"];
 }
